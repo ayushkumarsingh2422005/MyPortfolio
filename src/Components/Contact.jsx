839b23b8dc76643f8f7a2365b4f13f6c2b2e2dba
ayush kumar <ayush2422005@gmail.com>
2024-06-21 00:11:43 +0530

    updates

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,6 +6,8 @@ import { styles } from "../styles";
 import EarthCanvas from "../Model/Earth";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
+import BallCanvas from "../Model/Ball";
+import icon from '../assets/icons/figma.png'
 
 const Contact = () => {
   const formRef = useRef();
@@ -78,7 +80,7 @@ const Contact = () => {
         <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className='mt-12 flex flex-col gap-8'
+          className='mt-12 flex flex-col gap-8 text-gray-700'
         >
           <label className='flex flex-col'>
             <span className='text-white font-medium mb-4'>Your Name</span>
@@ -88,7 +90,7 @@ const Contact = () => {
               value={form.name}
               onChange={handleChange}
               placeholder="What's your good name?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary rounded-lg outline-none border-none font-medium'
             />
           </label>
           <label className='flex flex-col'>
@@ -99,7 +101,7 @@ const Contact = () => {
               value={form.email}
               onChange={handleChange}
               placeholder="What's your web address?"
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary rounded-lg outline-none border-none font-medium'
             />
           </label>
           <label className='flex flex-col'>
@@ -110,25 +112,38 @@ const Contact = () => {
               value={form.message}
               onChange={handleChange}
               placeholder='What you want to say?'
-              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary rounded-lg outline-none border-none font-medium'
             />
           </label>
 
           <button
             type='submit'
-            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
+            className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-sm shadow-slate-700'
           >
             {loading ? "Sending..." : "Send"}
           </button>
         </form>
+        <div className='flex flex-row flex-wrap justify-center gap-10'>
+          <div className='w-14 h-14'>
+            <BallCanvas icon={icon} />
+          </div>
+          <div className='w-14 h-14'>
+            <BallCanvas icon={icon} />
+          </div>
+          <div className='w-14 h-14'>
+            <BallCanvas icon={icon} />
+          </div>
+        </div>
       </motion.div>
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+        className='xl:flex-1 xl:h-auto md:h-[500px] h-[325px]'
       >
         <EarthCanvas />
       </motion.div>
+
+
     </div>
   );
 };
